6b8c8b4f5278d318c8930c328bf71e0531ba6438
Darsh Chawla <darshdchawla@gmail.com>
2026-06-19 00:35:30 +0530

    five or three but not fifteen

--- if_else/fiveorthreebutnotfifteen.c
+++ if_else/fiveorthreebutnotfifteen.c
@@ -4,18 +4,21 @@ int main()
     int n;
     printf("Enter a number : ");
     scanf("%d", &n);
-    if (n % 5 == 0 || n % 3 == 0)
-    {
-        if(n%15!=0){
-        printf("The number is divisible by 5 or 3 ");
-    }
-        else{
-            printf("The number is divisible by 15 ");
-        }
+    // if (n % 5 == 0 || n % 3 == 0)
+    // {
+    //     if(n%15!=0){
+    //     printf("The number is divisible by 5 or 3 but not fifteen ");
+    // }
+    //     else{
+    //         printf("The number is divisible by 15 ");
+    //     }
+    // }
+    if((n%5==0 || n%3==0) && n%15!=0){
+        printf("The number is divisible by 5 and 3 but not fifteen");
     }
     else
     {
-        printf("The number is not divisible by 5 or 3");
+        printf("The number is not matching the required  condition");
     }
     return 0;
 }
