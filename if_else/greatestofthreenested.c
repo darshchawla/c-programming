530c09881c4b479ca06771712b8a9a480fcacc6a
Darsh Chawla <darshdchawla@gmail.com>
2026-06-19 16:13:01 +0530

    greatest of three nested

---
+++ if_else/greatestofthreenested.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter 1st number : ");
+    scanf("%d",&a);
+    printf("Enter 2nd number : ");
+    scanf("%d",&b);
+    printf("Enter 3rd number : ");
+    scanf("%d",&c);
+    if(a>b){ // b is out of race
+        if(a>c)
+            printf("%d is greatest",a);
+        else // a<c
+            printf("%d is greatest",c);
+    }
+    else{ // b>a -> a ab sabse bada to nahi hai
+        if(b>c)
+            printf("%d is greatest",b);
+            else // c>b
+                printf("%d is greatest",c);
+    }
+    return 0;
+}
