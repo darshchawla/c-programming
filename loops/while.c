98d34cd7c48d80b1fdb868fda4cdc6a1e6fc748d
Darsh Chawla <darshdchawla@gmail.com>
2026-06-21 14:33:09 +0530

    Post/Pre - Increment/Decrement

--- loops/while.c
+++ loops/while.c
@@ -1,8 +1,10 @@
-#include <stdio.h> 
-int main(){
+#include <stdio.h>
+int main()
+{
     int i = 1;
-    while(i<101){ //while loop ke andar sirf condition hi likhi hoti hai, aur ye tabhi use karte hai jab hame jyada cheeze na pata ho 
-        printf("%d ",i);
+    while (i < 101)
+    { // while loop ke andar sirf condition hi likhi hoti hai, aur ye tabhi use karte hai jab hame jyada cheeze na pata ho
+        printf("%d ", i);
         i++;
     }
     return 0;
