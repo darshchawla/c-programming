738589447a556e50a413f22f7777a960db6089a3
Darsh Chawla <darshdchawla@gmail.com>
2026-06-23 09:32:50 +0530

    solid square

--- patternprinting/solidsquare.c
+++ patternprinting/solidsquare.c
@@ -8,10 +8,10 @@ int main()
     printf("Enter number of columns : ");
     scanf("%d", &m);
     for (int i = 1; i <= n; i++)
-    { // outer loop -> no. of lines
+    {
         for (int i = 1; i <= m; i++)
-        { // inner loop -> no. of stars in each line
-            printf("*");
+        {
+            printf("* ");
         }
         printf("\n");
     }
