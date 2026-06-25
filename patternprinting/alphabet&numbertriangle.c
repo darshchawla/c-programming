556fa4469f53ec58b86d96d52eae54445f4e8026
Darsh Chawla <darshdchawla@gmail.com>
2026-06-25 09:15:33 +0530

    star triangle mast

--- patternprinting/alphabet&numbertriangle.c
+++ patternprinting/alphabet&numbertriangle.c
@@ -6,15 +6,18 @@ int main()
     scanf("%d", &n);
     for (int i = 1; i <= n; i++)
     {
-        int a =1;
-        for(int j=1;j<=i;j++){
-            if(i%2!=0){
-                printf("%d ",j);
+        int a = 1;
+        for (int j = 1; j <= i; j++)
+        {
+            if (i % 2 != 0)
+            {
+                printf("%d ", j);
             }
-            else{
-                int d =a +64;
-                char ch=(char)d;
-                printf("%c ",ch);
+            else
+            {
+                int d = a + 64;
+                char ch = (char)d;
+                printf("%c ", ch);
                 a++;
             }
         }
