43252473e7ebc9b282f3564f5aa87e9c7a010a08
Darsh Chawla <darshdchawla@gmail.com>
2026-06-25 16:59:19 +0530

    alphabet triangle mast

---
+++ patternprinting/alphabetpyramidmast.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter number of lines : ");
+    scanf("%d", &n);
+    int nsp = n - 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int q = 1; q <= nsp; q++)
+        {
+            printf(" ");
+        }
+        nsp--;
+        for (int j = 1; j <= i; j++)
+        {
+            char ch = (char)(j+64);
+            printf("%c", ch);
+        }
+        int a = i - 1;
+        for (int k = 1; k <= i - 1; k++)
+        {
+            char ch = (char)(a+64);
+            printf("%c", ch);
+            a--;
+        }
+        printf("\n");
+    }
+    return 0;
+}
