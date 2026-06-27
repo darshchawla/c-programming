d388c27f890e0397f0c594271a608752992a798b
Darsh Chawla <darshdchawla@gmail.com>
2026-06-27 10:25:04 +0530

    pascal triangle without using nCr function

---
+++ functions/pascaltrianglewithoutnCrfunction.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter n : ");
+    scanf("%d", &n);
+    for (int i = 0; i <= n; i++)
+    {
+        int first = 1;
+        for (int a = 0; a <= n - i; a++)
+        {
+            printf(" ");
+        }
+        for (int j = 0; j <= i; j++)
+        {
+            printf("%d ", first);
+            first = first * (i - j) / (j + 1);
+        }
+
+        printf("\n");
+    }
+    return 0;
+}
