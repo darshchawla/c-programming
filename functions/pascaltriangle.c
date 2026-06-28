ac2af7b5de802bb1484557375b9dfc90026cae28
Darsh Chawla <darshdchawla@gmail.com>
2026-06-28 13:26:55 +0530

    pascal triangle

--- functions/pascaltriangle.c
+++ functions/pascaltriangle.c
@@ -8,8 +8,9 @@ int factorial(int x)
     }
     return fact;
 }
-    int combination(int n,int r){
-    int ncr = factorial(n) / (factorial(r) * factorial(n-r));
+int combination(int n, int r)
+{
+    int ncr = factorial(n) / (factorial(r) * factorial(n - r));
     return ncr;
 }
 int main()
@@ -17,13 +18,16 @@ int main()
     int n;
     printf("Enter n : ");
     scanf("%d", &n);
-    for(int i=0;i<=n;i++){
-        for(int a =0;a<=n-i;a++){
+    for (int i = 0; i <= n; i++)
+    {
+        for (int a = 0; a <= n - i; a++)
+        {
             printf(" ");
         }
-        for(int j=0;j<=i;j++){
-            int icj = combination(i,j);
-            printf("%d ",icj);
+        for (int j = 0; j <= i; j++)
+        {
+            int icj = combination(i, j);
+            printf("%d ", icj);
         }
         printf("\n");
     }
