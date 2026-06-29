b59eff2b114c85ca66266e35c99e18b300dffa30
Darsh Chawla <darshdchawla@gmail.com>
2026-06-29 19:08:25 +0530

    stair path 2 jumps max

---
+++ recursion/stairpathcount.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int stair(int n)
+{
+    // if (n == 1)
+    //     return 1;
+    // if (n == 2)
+    //     return 2;
+    if(n==1 || n==2) return n;
+    int totalWays = stair(n - 1) + stair(n - 2);
+    return totalWays;
+}
+int main()
+{
+    int n;
+    printf("Enter a number : ");
+    scanf("%d", &n);
+    int ways = stair(n);
+    printf("The total number of ways are : %d", ways);
+    return 0;
+}
