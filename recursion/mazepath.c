80d3b326c0ddab9c04b165587fa65aab7fbe560a
Darsh Chawla <darshdchawla@gmail.com>
2026-06-30 14:17:14 +0530

    maze path 2 parameter

--- recursion/mazepath.c
+++ recursion/mazepath.c
@@ -1,29 +1,35 @@
 #include <stdio.h>
-int maze (int cr , int cc , int er , int ec){
+int maze(int cr, int cc, int er, int ec)
+{
     int rightWays = 0;
     int downWays = 0;
-    if(cr==er && cc==ec) return 1;
-    if (cr==er) { // only rightWays call
-        rightWays += maze (cr,cc+1,er,ec);
+    if (cr == er && cc == ec)
+        return 1;
+    if (cr == er)
+    { // only rightWays call
+        rightWays += maze(cr, cc + 1, er, ec);
     }
-    if(cc==er){ // only downWays call 
-        downWays += maze (cr+1,cc,er,ec);
+    if (cc == er)
+    { // only downWays call
+        downWays += maze(cr + 1, cc, er, ec);
     }
-    if(cr<er && cc<ec){
-        rightWays += maze (cr,cc+1,er,ec);
-        downWays += maze (cr+1,cc,er,ec);
+    if (cr < er && cc < ec)
+    {
+        rightWays += maze(cr, cc + 1, er, ec);
+        downWays += maze(cr + 1, cc, er, ec);
     }
-    int totalWays = rightWays + downWays; 
+    int totalWays = rightWays + downWays;
     return totalWays;
 }
-int main (){
+int main()
+{
     int n;
     printf("Enter number of rows of the maze : ");
-    scanf("%d",&n);
+    scanf("%d", &n);
     int m;
     printf("Enter number of columns of the maze : ");
-    scanf("%d",&m);
-    int noOfWays = maze (1,1,n,m);
-    printf("The total number of ways are : %d",noOfWays);
+    scanf("%d", &m);
+    int noOfWays = maze(1, 1, n, m);
+    printf("The total number of ways are : %d", noOfWays);
     return 0;
 }
---
+++ recursion/mazepath2parameter.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int maze2 (int n , int m){
+    int rightWays = 0;
+    int downWays = 0;
+    if (n==1 && m==1) return 1;
+    if(n==1){
+        rightWays += maze2 (n,m-1);
+    }
+    if(m==1){
+        downWays += maze2 (n-1,m);
+    }
+    if(n>1 && m>1){
+        rightWays += maze2 (n,m-1);
+        downWays += maze2 (n-1,m);
+    }
+    int totalWays = rightWays + downWays;
+    return totalWays;
+}
+int main (){
+    int n;
+    printf("Enter number of rows of the maze : ");
+    scanf("%d",&n);
+    int m;
+    printf("Enter number of columns of the maze : ");
+    scanf("%d",&m);
+    int noOfWays = maze2 (n,m);
+    printf("The total number of ways are : %d",noOfWays);
+    return 0;
+}
