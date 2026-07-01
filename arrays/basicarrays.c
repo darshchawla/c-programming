379c40b7fcad89ec0d5ef8270e82ff16b200a538
Darsh Chawla <darshdchawla@gmail.com>
2026-07-01 18:41:51 +0530

    print reverse array

--- arrays/basicarrays.c
+++ arrays/basicarrays.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
-int main (){
+int main()
+{
     int arr[5];
-    for(int i=0;i<=4;i++){
-        int a = i+1;
-        printf("\nEnter element number %d : ",a);
-        scanf("%d ",&arr[i]);
+    for (int i = 0; i <= 4; i++)
+    {
+        int a = i + 1;
+        printf("\nEnter element number %d : ", a);
+        scanf("%d", &arr[i]);
     }
-    for(int i=0;i<5;i++){
-        printf("%d ",arr[i]);
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d ", arr[i]);
     }
     return 0;
 }
