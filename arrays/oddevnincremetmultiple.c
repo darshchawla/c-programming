6e7a2a0c12cc638c8980213c095d4d6b903975ba
Darsh Chawla <darshdchawla@gmail.com>
2026-07-02 20:52:37 +0530

    odd even increment multiple

---
+++ arrays/oddevnincremetmultiple.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    int arr[7];
+    for (int i = 0; i <= 6; i++)
+    {
+        int a = i + 1;
+        printf("\nEnter element number %d : ", a);
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i <= 6; i++)
+    {
+        if (i % 2 != 0)
+        {
+            arr[i] = arr[i] * 2;
+        }
+        if (i % 2 == 0)
+        {
+            arr[i] = arr[i] + 10;
+        }
+    }
+    for (int i = 0; i <= 6; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
