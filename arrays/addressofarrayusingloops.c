7361484adafa065091aa0a0b43d6b124feb6e564
Darsh Chawla <darshdchawla@gmail.com>
2026-07-02 14:43:28 +0530

    garbage values

--- arrays/addressofarrayusingloops.c
+++ arrays/addressofarrayusingloops.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-int main (){
-    int arr[5] = {5,7,3,7,9};
-    for(int i =0;i<=4;i++){
-        printf("%p\n",&arr[i]);
+int main()
+{
+    int arr[5] = {5, 7, 3, 7, 9};
+    for (int i = 0; i <= 4; i++)
+    {
+        printf("%p\n", &arr[i]);
     }
     return 0;
 }
