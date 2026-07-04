db0b301b9cefa6d45561289c73e19506af0bcba6
Darsh Chawla <darshdchawla@gmail.com>
2026-07-04 22:53:55 +0530

    sum of two integers

---
+++ Basic/sumofintegers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (){
+    int a;
+    printf("\nEnter first number : ");
+    scanf("%d",&a);
+    int b;
+    printf("\nEnter first number : ");
+    scanf("%d",&b);
+    int sum = 0;
+    sum = a + b;
+    printf("\nThe sum of the given integers is : %d\n",sum);
+    return 0;
+}
