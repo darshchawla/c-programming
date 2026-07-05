73d27df205629a9e984c926e5a0a05e9814e2834
Darsh Chawla <darshdchawla@gmail.com>
2026-07-05 11:09:34 +0530

    sum of matrix

---
+++ 2D arrays/sumofmatrix.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main(){
+    int r;
+    printf("\nEnter number of rows : ");
+    scanf("%d", &r);
+    int c;
+    printf("\nEnter number of columns : ");
+    scanf("%d", &c);
+    printf("\nEnter all the elements\n");
+    int arr[r][c];
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    printf("\n");
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    int sum = 0;
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            sum = sum  + arr[i][j];
+        }
+    }
+    printf("The sum of the given matrix is : %d",sum);
+    return 0;
+}
