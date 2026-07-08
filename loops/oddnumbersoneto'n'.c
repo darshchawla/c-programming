23a9845a4ba443d8f8f4caf27e4bc739a1ae01e2
Darsh Chawla <darshdchawla@gmail.com>
2026-07-08 13:17:26 +0530

    odd numbers one to n

---
+++ loops/oddnumbersoneto'n'.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a number : ");
+    scanf("%d", &n);
+    printf("\nThe odd numbers from one to n is : ");
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 != 0)
+        {
+            printf("%d ", i);
+        }
+    }
+    return 0;
+}
