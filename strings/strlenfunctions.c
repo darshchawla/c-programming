246376f66cf661bb6f411e871cfc17aab5a28557
Darsh Chawla <darshdchawla@gmail.com>
2026-07-15 23:17:59 +0530

    strlen function

---
+++ strings/strlenfunctions.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char *str = "Darsh Chawla.";
+    int a = strlen(str);
+    printf("\nThe length of the string is : %d\n", a);
+    return 0;
+}
