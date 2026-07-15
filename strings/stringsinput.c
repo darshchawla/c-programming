64dbc57ac4c7b94d1bfca765f2c8893b98b61317
Darsh Chawla <darshdchawla@gmail.com>
2026-07-15 11:30:55 +0530

    made some changes

--- strings/stringsinput.c
+++ strings/stringsinput.c
@@ -2,7 +2,7 @@
 int main()
 {
     int s;
-    printf("\nEnter the size of the array : ");
+    printf("\nEnter the size of the string : ");
     scanf("%d", &s);
     char arr[s];
     for (int i = 0; i <= s - 1; i++)
@@ -11,7 +11,7 @@ int main()
         printf("\nEnter element number %d : ", a);
         scanf(" %c", &arr[i]);
     }
-    printf("\nThe array of size %d is : ", s);
+    printf("\nThe string of size %d is : ", s);
     for (int i = 0; i <= s - 1; i++)
     {
         printf("%c ", arr[i]);
