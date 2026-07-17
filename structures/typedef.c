464623cc9b5f828607da5909aec3099fd18c6215
Darsh Chawla <darshdchawla@gmail.com>
2026-07-17 14:52:02 +0530

    typedef

---
+++ structures/typedef.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+typedef float realNumber;
+typedef int Integer;
+int main()
+{
+    Integer x = 7;
+    realNumber y = 3.1415;
+    printf("%d\n", x);
+    printf("%f", y);
+    return 0;
+}
